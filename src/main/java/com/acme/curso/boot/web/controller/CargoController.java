0c903ae86cfa8ef0cb97f7da3682143b0ab4fff6
Rogério de Lima Sousa <rogeriothe@gmail.com>
2018-10-22 18:11:03 -0300

    secao 09

--- src/main/java/com/acme/curso/boot/web/controller/CargoController.java
+++ src/main/java/com/acme/curso/boot/web/controller/CargoController.java
@@ -1,25 +1,83 @@
 package com.acme.curso.boot.web.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.acme.curso.boot.domain.Cargo;
+import com.acme.curso.boot.domain.Departamento;
+import com.acme.curso.boot.services.CargoService;
+import com.acme.curso.boot.services.DepartamentoService;
 
 @Controller
 @RequestMapping("/cargos")
 public class CargoController {
-	
+
+	@Autowired
+	private CargoService cargoService;
+
+	@Autowired
+	private DepartamentoService departamentoService;
+
 	@GetMapping("/cadastrar")
-	public String cadastrar()
-	{
-		//caminho do template
+	public String cadastrar(Cargo cargo) {
+		// caminho do template
 		return "/cargo/cadastro";
 	}
-	
+
 	@GetMapping("/listar")
-	public String listar()
-	{
-		//caminho do template		
+	public String listar(ModelMap model) {
+		model.addAttribute("cargos", cargoService.buscarTodos());
+		/* caminho do template */
 		return "/cargo/lista";
+	}
+
+	@PostMapping("/salvar")
+	public String salver(Cargo cargo, RedirectAttributes attr) {
+		cargoService.salvar(cargo);
+
+		attr.addFlashAttribute("success", "Cargo inserido com sucesso.");
+
+		return "redirect:/cargos/cadastrar";
+	}
+
+	@GetMapping("/editar/{id}")
+	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+		model.addAttribute("cargo", cargoService.buscarPorId(id));
+		return "cargo/cadastro";
+	}
+
+	@PostMapping("/editar")
+	public String editar(Cargo cargo, RedirectAttributes attr) {
+		cargoService.editar(cargo);
+		attr.addFlashAttribute("success", "Registro atualizado com seucesso.");
+		return "redirect:/cargos/cadastrar";
+	}
+
+	@GetMapping("/excluir/{id}")
+	public String excluir(@PathVariable("id") Long id, RedirectAttributes attr) {
+
+		if (cargoService.cargoTemFuncionarios(id)) {
+			attr.addFlashAttribute("fail", "Cargo não excluído. Tem funcionário(s) vinculado(s).");
+		} else {
+			cargoService.excluir(id);
+			attr.addFlashAttribute("success", "Cargo excluído com sucesso.");
+		}
+
+		return "redirect:/cargos/listar";
+	}
+
+	@ModelAttribute("departamentos")
+	public List<Departamento> listaDepartamentos() {
+		return departamentoService.buscarTodos();
 	}
 
 }
